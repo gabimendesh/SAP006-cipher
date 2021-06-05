bc9f409f94b9d5788e0e08767e26d5061772cd6f
Gabrielly Mendes <silvamgabi@hotmail.com>
2021-06-05 17:06:46 -0300

    cipher.js encode adicionado

--- src/cipher.js
+++ src/cipher.js
@@ -1,4 +1,4 @@
-/* cifra de césar
+/* cifra de césar //  [A-Z = 65-90]
  
  A  B  C  D  E  F  G  H  I  J  K   L   M  
  0  1  2  3  4  5  6  7  8  9  10  11  12 
@@ -8,18 +8,22 @@
  
  */
 
-const cipher = {
-  encode: (offset, string) => {
-    //nossa function recebe dois parâmentros string=texto para ser cifrado e offset= o número de posições que queremos mover para a direita no alfabeto ASCII
-    let resultado = ""; //guardar o resultado numa variável
+//encode ASCII
+
+//codASCII => cod0a25 => offset => giro => codASCII
+
+const cipher = (offset, string) => {//nossa function recebe dois parâmentros string=texto para ser cifrado e offset= o número de posições que queremos mover para a direita no alfabeto ASCII
+ let resultado = "", encode;//guardar o resultado numa variável
     for (let i = 0; i < string.length; i++) {
-      //criando loopind e transformando nossa string e increment
+      //criando loop e transformando nossa string e increment
       if (string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90) {
-        //Se nossa string em charCodeAt valor inicial for maior ou igual a 65 ("A") AND menor ou igual a 90 ("Z")
-        encode = ((string.charCodeAt(i) - 65 + offset); // Então numa variável traz a string em posição inicial (65) tirando -65 = 0 adicionando o offset(deslocamento)
+        //Se nossa string em charCodeAt valor inicial for maior ou igual a 65 ("A") AND menor ou igual a 90 ("Z") executa isso:
+         encode = ((string.charCodeAt(i) - 65 + offset) % 26) + 65; // Na variável traz a string em posição inicial (65) tirando -65 deixando ele igual a 0 adicionando o offset(deslocamento) + o módulo tamanho do alfabeto para fazer o loop / e adicionando 65 para encriptar com o código da primeira letra     
       }
-    }
-  },
-};
+      resultado+=String.fromCharCode(encode)
+    }    
+  return resultado
+}
+
 
 export default cipher;
