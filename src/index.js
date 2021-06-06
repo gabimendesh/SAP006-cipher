aa2b167dc350200ebe632b05f8fc0d5f8ec1cdd5
Gabrielly Mendes <silvamgabi@hotmail.com>
2021-06-05 21:17:09 -0300

    html alterado e index.js buttons funcionando ao serem clicados

--- src/index.js
+++ src/index.js
@@ -2,16 +2,22 @@ import cipher from "./cipher.js";
 
 console.log(cipher);
 
-/*let cifrarButton1 = document.getElementById("cifrar-button");
-
-const myText = document.querySelector("myText").value;
+//buttons 
 
 function cifrar() {
-  let resultado = myText.value;
-  document.getElementById("codeText").innerHTML = resultado;
-}
+ const texto = document.getElementById("myText").value
+ document.getElementById("codeText").value = texto
 
-let cifrarButton = document.getElementById("cifrar-button");
+} 
+let cifrarButton = document.getElementById("cifrar");
 cifrarButton.addEventListener("click", cifrar);
 
-cifrarButton1.addEventListener("input", cifrar);*/
+
+function decifrar() {
+  const decifrado = document.getElementById("codeText").value
+  document.getElementById("myText").value = decifrado
+  
+ } 
+
+let decifrarButton = document.getElementById("decifrar");
+decifrarButton.addEventListener("click", decifrar);
